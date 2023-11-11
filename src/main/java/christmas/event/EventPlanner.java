84931e946f07aec62ffd3e51947f23a8ce16b854
lzlkolo <lzlko1209@gmail.com>
2023-11-11 14:10:53 +0900

    refactor: extract constant and method in EventPlanner

--- src/main/java/christmas/event/EventPlanner.java
+++ src/main/java/christmas/event/EventPlanner.java
@@ -8,17 +8,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class EventPlanner {
+    private final static List<Integer> SPECIAL_DATES = List.of(3, 10, 17, 24, 25, 31);
+
     private final LocalDate visitDate;
     private final Order order;
+    private final List<Event> events;
 
     public EventPlanner(LocalDate visitDate, Order order) {
         this.visitDate = visitDate;
         this.order = order;
+        this.events = initializeEvents();
     }
 
     public int calculateTotalDiscount() {
         int totalDiscount = 0;
-        List<Event> events = addEventsByCondition();
+        List<Event> events = initializeEvents();
 
         for (Event event : events) {
             totalDiscount += event.calculateDiscount(order);
@@ -27,20 +31,31 @@ public class EventPlanner {
         return totalDiscount;
     }
 
-    private List<Event> addEventsByCondition() {
+    private List<Event> initializeEvents() {
         List<Event> events = new ArrayList<>();
         events.add(new ChristmasEvent(visitDate));
+        addWeekdayEvent();
+        addWeekendEvent();
+        addSpecialEvent();
+        return events;
+    }
 
+    private void addWeekdayEvent() {
         if (isWeekday(visitDate)) {
             events.add(new WeekdayEvent());
         }
+    }
+
+    private void addWeekendEvent() {
         if (isWeekend(visitDate)) {
             events.add(new WeekendEvent());
         }
+    }
+
+    private void addSpecialEvent() {
         if (isSpecialDate(visitDate)) {
             events.add(new SpecialEvent());
         }
-        return events;
     }
 
     private boolean isWeekday(LocalDate date) {
@@ -54,7 +69,6 @@ public class EventPlanner {
     }
 
     private boolean isSpecialDate(LocalDate date) {
-        List<Integer> specialDates = new ArrayList<>(List.of(3, 10, 17, 24, 25, 31));
-        return specialDates.contains(date.getDayOfMonth());
+        return SPECIAL_DATES.contains(date.getDayOfMonth());
     }
 }

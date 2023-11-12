75d91e617aa9357ccfe2989a5c8f3dfd3c85121f
lzlkolo <lzlko1209@gmail.com>
2023-11-13 07:25:20 +0900

    test: add test for input order

--- src/test/java/christmas/ui/InputViewTest.java
+++ src/test/java/christmas/ui/InputViewTest.java
@@ -1,24 +1,62 @@
 package christmas.ui;
 
+import camp.nextstep.edu.missionutils.Console;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Map;
 
+import static java.util.Map.entry;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class InputViewTest {
-    @DisplayName("방문 날짜 입력 테스트")
+    InputView inputView;
+
+    @BeforeEach
+    public void setUp() {
+        inputView = new InputView();
+    }
+
+    @DisplayName("방문 날짜 입력 후 반환 값 테스트")
     @Test
     public void inputViewTestByVisitDate() {
-        String input = "15";
-        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        System.setIn(createUserInput("15"));
 
-        InputView inputView = new InputView();
         int visitDate = inputView.getVisitDate();
 
         assertEquals(15, visitDate);
 
         System.setIn(System.in);
+    }
+
+    @DisplayName("주문 입력 후 반환 값 테스트")
+    @Test
+    public void inputViewTestByOrder() {
+        String input = "크리스마스파스타-1,레드와인-1";
+        System.setIn(createUserInput(input));
+
+        Map<String, Integer> orderItems = inputView.getOrder();
+
+        Map<String, Integer> expectedOrderItems = Map.ofEntries(
+                entry("크리스마스파스타", 1),
+                entry("레드와인", 1)
+        );
+
+        assertEquals(expectedOrderItems, orderItems);
+
+        System.setIn(System.in);
+    }
+
+    InputStream createUserInput(String input) {
+        return new ByteArrayInputStream(input.getBytes());
+    }
+
+    @AfterEach
+    public void closeConsole() {
+        Console.close();
     }
 }
